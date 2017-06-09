64d58b8aeab36acf6c0d3262baa02389561d142d
JeonWooJin <dnwls3409@gamil.com>
2017-06-09 20:15:06 +0900

    commit 5
    스피너 이용 드롭다운 메뉴

--- app/src/main/java/com/example/jeonwoojin/captsone/MainActivity.java
+++ app/src/main/java/com/example/jeonwoojin/captsone/MainActivity.java
@@ -5,15 +5,80 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 
 public class MainActivity extends AppCompatActivity {
+    String[] SpinnerArr1 = {"전체","취업설명회","리크루팅"};
+    String[] SpinnerArr2 = {"캠퍼스전체","인사캠","자과캠"};
+    String[] SpinnerArr3 = {"기업명+제목","기업명","제목"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                SpinnerArr1
+        );
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                SpinnerArr2
+        );
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                SpinnerArr3
+        );
+
+
+        spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
+        spinner3.setAdapter(adapter3);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
     void onSKKUlinkButtonClicked(View v){
