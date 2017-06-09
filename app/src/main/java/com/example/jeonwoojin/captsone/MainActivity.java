dab6511c19c064c8709fb2362cc8a86ef7b52be8
JeonWooJin <dnwls3409@gamil.com>
2017-06-09 14:24:57 +0900

    commit 세번째
    Job.skku.edu 버튼 연동

--- app/src/main/java/com/example/jeonwoojin/captsone/MainActivity.java
+++ app/src/main/java/com/example/jeonwoojin/captsone/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jeonwoojin.captsone;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+    void onSKKUlinkButtonClicked(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://job.skku.edu"));
+        startActivity(i);
     }
 }
